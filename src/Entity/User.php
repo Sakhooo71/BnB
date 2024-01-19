4c45ad2ec4a4ac22b9ae4ad963088aae96ac83bd
hamady <sakhooo71.com>
2024-01-19 14:26:40 +0100

    update bdd, App.css create entitéFavorite create RoomController create AppFixture update de base&city.html.twig

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -67,11 +67,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
+    #[ORM\OneToMany(mappedBy: 'traveler', targetEntity: Favorite::class, orphanRemoval: true)]
+    private Collection $favorites;
+
+   
+
     public function __construct()
     {
         $this->rooms = new ArrayCollection();
         $this->reviews = new ArrayCollection();
         $this->bookings = new ArrayCollection();
+        $this->favorites = new ArrayCollection();
+        
     }
 
     public function getId(): ?int
@@ -341,4 +348,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    /**
+     * @return Collection<int, Favorite>
+     */
+    public function getFavorites(): Collection
+    {
+        return $this->favorites;
+    }
+
+    public function addFavorite(Favorite $favorite): static
+    {
+        if (!$this->favorites->contains($favorite)) {
+            $this->favorites->add($favorite);
+            $favorite->setTraveler($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFavorite(Favorite $favorite): static
+    {
+        if ($this->favorites->removeElement($favorite)) {
+            // set the owning side to null (unless already changed)
+            if ($favorite->getTraveler() === $this) {
+                $favorite->setTraveler(null);
+            }
+        }
+
+        return $this;
+    }
+
+    
 }

9988e584f2f9edee13f8c8b71ffd300c0d05fc2f
hamady <sakhooo71.com>
2024-01-22 11:25:47 +0100

    update room.cover

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,21 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-#[Route('/r')]
+#[Route('/r')] // prefix all room routes with /r
 class RoomController extends AbstractController
 {
     #[Route('/', name: 'app_room')]
     public function index(
-        RoomRepository $roomRepository
+        RoomRepository $roomRepository,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response
     {
+        $pagination = $paginator->paginate(
+            $roomRepository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            12 /*limit per page*/
+        );
         return $this->render('room/index.html.twig', [
-            'rooms' => $roomRepository-> findAll()
+            'rooms' => $pagination,
+            'hostRooms' => $roomRepository->findBy(
+                ['host' => $this->getUser()]
+                )
         ]);
     }
 }
